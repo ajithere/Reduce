1c38ec69b27078ea8f23746f6dc0403774d56f57
Ajit <amp.ajit@gmail.com>
2017-08-24 13:33:48 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#############################################
+
+This is a basic image size reducer.
+
+Run the command python reduce.py. if run without inputs, it takes all images from the current source directory and
+dumps it into destination folder. It expects the 2 folders to be present.
+
+You can provide the source and destination folders as input parameters as well. 
---
+++ reduce.py
@@ -0,0 +1,22 @@
+import os,sys
+import PIL
+from PIL import Image
+
+if len(sys.argv) == 3:
+	path = sys.argv[1]
+	outpath = sys.argv[2]
+elif len(sys.argv) == 1:
+	path = "./source/"
+	outpath = "./destination"
+else:
+	print "Invaild entries..."
+	 
+
+for filename in os.listdir(path):
+	print filename
+	basewidth = 1024
+	img = Image.open(path + "/" + filename)
+	wpercent = (basewidth/float(img.size[0]))
+	hsize = int((float(img.size[1])*float(wpercent)))
+	img = img.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
+	img.save(outpath + "/" + filename)
